071c58f275a84366cf82013c4d6bf28fe34af76c
AndrewDoerfler2 <andrew.doerfler@gmail.com>
2024-01-13 12:49:03 -0500

    routing

--- foxen-onboarding/src/App.tsx
+++ foxen-onboarding/src/App.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNavigationBar from './Components/TopNavigation/Navigation';
 import OrgChartPage from './Pages/Documentation/OrgChart/OrgChartPage';
 import PageContainer from './Components/PageContainer/PageContainer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/Landing/Landing';
 import HelpfulLinksPage from './Pages/HelpfulLinks/HelpfulLinksPage';
 import DataIngestFlowPage from './Pages/Documentation/DataIngestFlow/DataIngestFlowPage';
@@ -16,7 +16,7 @@ function App() {
       <div>
         <TopNavigationBar />
         <PageContainer>
-          <BrowserRouter basename='/Foxen-Onboarding'>
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<LandingPage />} />
               <Route path="/org-chart" element={<OrgChartPage />} />
@@ -24,7 +24,7 @@ function App() {
               <Route path="/data-ingest-flow" element={<DataIngestFlowPage />} />
               <Route path="/domain-model" element={<DomainModelPage />} />
             </Routes>
-          </BrowserRouter>
+          </BrowserRouter >
         </PageContainer>
         
       </div>
